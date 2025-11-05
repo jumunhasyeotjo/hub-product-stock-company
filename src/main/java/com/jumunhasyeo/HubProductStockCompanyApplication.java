498b898344802a40bb9038b80c571e8287bc9451
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-11-05 22:13:19 +0900

    feat: globalException, 공통 응답, 비즈니스 예외 생성

--- src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
+++ src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
@@ -1,13 +1,7 @@
-package com.jumunhasyeo.hub;
+package com.jumunhasyeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 @SpringBootApplication
 public class HubProductStockCompanyApplication {
---
+++ src/main/java/com/jumunhasyeo/hub/common/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.jumunhasyeo.hub.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    // 공통
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E001", "서버 에러가 발생했습니다."),
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "E002", "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E003", "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "E004", "접근이 거부되었습니다."),
+    CREATE_VALIDATE_EXCEPTION(HttpStatus.BAD_REQUEST,"E005", "객체 생성에 실패했습니다.")
+
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
