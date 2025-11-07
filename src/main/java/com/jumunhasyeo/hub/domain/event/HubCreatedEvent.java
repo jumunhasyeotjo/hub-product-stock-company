8c0fa1dab9dd40b405058633c5826564bfe28b70
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-11-07 19:05:52 +0900

    refactor: remove occurredAt from HubCreatedEvent constructor

--- src/main/java/com/jumunhasyeo/hub/domain/event/HubCreatedEvent.java
+++ src/main/java/com/jumunhasyeo/hub/domain/event/HubCreatedEvent.java
@@ -12,21 +12,18 @@ public final class HubCreatedEvent extends HubDomainEvent {
     private final UUID hubId;
     private final String name;
     private final Address address;
-    private final LocalDateTime occurredAt;
 
-    public HubCreatedEvent(UUID hubId, String name, Address address, LocalDateTime occurredAt) {
+    public HubCreatedEvent(UUID hubId, String name, Address address) {
         this.hubId = hubId;
         this.name = name;
         this.address = address;
-        this.occurredAt = occurredAt;
     }
 
     public static HubCreatedEvent of(Hub hub) {
         return new HubCreatedEvent(
                 hub.getHubId(),
                 hub.getName(),
-                hub.getAddress(),
-                LocalDateTime.now()
+                hub.getAddress()
         );
     }
 }
