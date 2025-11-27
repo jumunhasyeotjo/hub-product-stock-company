9fc35b9d111ef6c230e856967b870c9106c168f7
RaeHwan Park <prh0108@naver.com>
2025-11-27 21:16:37 +0900

    feat: passport 일부 적용

--- src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
+++ src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
@@ -1,8 +1,16 @@
 package com.jumunhasyeo;
 
+import com.library.passport.config.WebMvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
+@ComponentScan(basePackages = {
+        "com.jumunhasyeo",
+        "com.library.passport"
+})
+@Import(WebMvcConfig.class)
 @SpringBootApplication
 public class HubProductStockCompanyApplication {
 
