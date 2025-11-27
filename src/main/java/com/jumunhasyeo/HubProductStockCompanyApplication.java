a967448e5b471f1a5c2abed37bcd9df6b7564cc0
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-11-27 21:48:44 +0900

    feat: integrate passport library and configure WebMvc settings

--- src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
+++ src/main/java/com/jumunhasyeo/HubProductStockCompanyApplication.java
@@ -1,16 +1,8 @@
 package com.jumunhasyeo;
 
-import com.library.passport.config.WebMvcConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-@ComponentScan(basePackages = {
-        "com.jumunhasyeo",
-        "com.library.passport"
-})
-@Import(WebMvcConfig.class)
 @SpringBootApplication
 public class HubProductStockCompanyApplication {
 
