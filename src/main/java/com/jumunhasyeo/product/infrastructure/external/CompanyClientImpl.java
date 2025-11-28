6bb00a23632724f471aa2bc6662830697be7c62b
RaeHwan Park <prh0108@naver.com>
2025-11-28 17:43:20 +0900

    feat: 테스트용 eureka 적용

--- src/main/java/com/jumunhasyeo/product/infrastructure/external/CompanyClientImpl.java
+++ src/main/java/com/jumunhasyeo/product/infrastructure/external/CompanyClientImpl.java
@@ -1,17 +1,21 @@
 package com.jumunhasyeo.product.infrastructure.external;
 
+import com.jumunhasyeo.company.application.CompanyService;
 import com.jumunhasyeo.product.application.service.CompanyClient;
-import org.springframework.cloud.openfeign.FeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class CompanyClientImpl implements CompanyClient {
+
+    private final CompanyService companyService;
 
     // Todo : Company 도메인 완성시 Internal 로 구현
     @Override
     public boolean existsCompany(UUID organizationId) {
-        return true;
+        return companyService.existsById(organizationId);
     }
 }
