853a20ee685606bd6f6d6a34f1f62509fb66387e
RaeHwan Park <prh0108@naver.com>
2025-12-08 16:16:53 +0900

    rename: Internal Client 이름 수정

--- src/main/java/com/jumunhasyeo/product/infrastructure/external/CompanyInternalClientImpl.java
+++ src/main/java/com/jumunhasyeo/product/infrastructure/external/CompanyInternalClientImpl.java
@@ -9,11 +9,10 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-public class CompanyClientImpl implements CompanyClient {
+public class CompanyInternalClientImpl implements CompanyClient {
 
     private final CompanyService companyService;
 
-    // Todo : Company 도메인 완성시 Internal 로 구현
     @Override
     public boolean existsCompany(UUID organizationId) {
         return companyService.existsById(organizationId);
