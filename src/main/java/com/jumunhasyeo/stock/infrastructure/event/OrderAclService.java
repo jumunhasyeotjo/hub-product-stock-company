9ecb27ec9bcfe3c9546c8580b72c52e2d8606b33
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-12-10 16:48:06 +0900

    fix: correct event type name from ORDER_CANCELED to ORDER_CANCELLED in OrderAclService

--- src/main/java/com/jumunhasyeo/stock/infrastructure/event/OrderAclService.java
+++ src/main/java/com/jumunhasyeo/stock/infrastructure/event/OrderAclService.java
@@ -12,7 +12,7 @@ public class OrderAclService {
     public String convert(String eventType) {
         return switch (eventType) {
             case "ORDER_ROLLEDBACK" -> ORDER_ROLLED_BACK_EVENT.getEventName();
-            case "ORDER_CANCELED" -> ORDER_CANCEL_EVENT.getEventName();
+            case "ORDER_CANCELLED" -> ORDER_CANCEL_EVENT.getEventName();
             case "ORDER_CREATED" -> ORDER_CREATED.getEventName();
             default -> throw new IllegalArgumentException();
         };
